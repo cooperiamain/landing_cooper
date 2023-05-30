d5bd4d5726d5e8725542614ce63a080f72a8635c
Luis <luiselias0247@gmail.com>
2023-05-30 16:09:00 -0500

    botones alfa

--- components/landing/hero.jsx
+++ components/landing/hero.jsx
@@ -2,26 +2,39 @@ import React from "react";
 import Link from "next/link";
 
 const Hero = () => {
-    return (
-        <header className='w-full flex justify-center items-center flex-col'>
-            <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-            <img src='/logo.svg' alt='Cooper' className='w-28 object-contain' />
-    
-            <Link href="https://tally.so/r/woD9GX" className="black_btn">Early Access</Link>
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src="/logo.svg" alt="Cooper" className="w-28 object-contain" />
 
-            </nav>
-    
-            <h1 className='head_text'>
-            Tu copiloto en la universidad <br className='max-md:hidden' />
-            <span className='orange_gradient '>Chatea, resume y practica</span>
-            </h1>
-            <h2 className='desc'>
-            Todas tus lecturas en un solo lugar.
-            <br />
-            Una experiencia de aprendizaje como nunca antes la habías vivido.
-            </h2>
-        </header>
-    );
+        <div className="flex space-x-4">
+          <Link
+            href="http://cooperpdf.com/register"
+            className="text-black flex justify-center items-center bg-white py-2 px-4 rounded-md"
+          >
+            Registrarme
+          </Link>
+
+          <Link
+            href="http://cooperpdf.com/login"
+            className="text-white flex justify-center items-center bg-black py-2 px-4 rounded-md"
+          >
+            ¡Iniciar sesión!
+          </Link>
+        </div>
+      </nav>
+
+      <h1 className="head_text">
+        Tu copiloto en la universidad <br className="max-md:hidden" />
+        <span className="orange_gradient ">Chatea, resume y practica</span>
+      </h1>
+      <h2 className="desc">
+        Todas tus lecturas en un solo lugar.
+        <br />
+        Una experiencia de aprendizaje como nunca antes la habías vivido.
+      </h2>
+    </header>
+  );
 };
 
 export default Hero;
